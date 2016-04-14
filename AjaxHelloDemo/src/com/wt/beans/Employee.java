aa3af9da93691073bc7bad2c65f13ee0ce2b1671
hohoTT <609029365@qq.com>
2016-04-14 20:06:52 +0800

    使用hibernate框架创建基于mysql的数据库的三张表，并添加juit测试，测试成功，数据库表创建成功

---
+++ AjaxHelloDemo/src/com/wt/beans/Employee.java
@@ -0,0 +1,34 @@
+package com.wt.beans;
+
+public class Employee {
+	
+	private Integer employee_id;
+	private String last_name;
+	private String email;
+	private double salary;
+	public Integer getEmployee_id() {
+		return employee_id;
+	}
+	public void setEmployee_id(Integer employee_id) {
+		this.employee_id = employee_id;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
---
+++ AjaxHelloDemo/src/com/wt/beans/Location.java
@@ -0,0 +1,21 @@
+package com.wt.beans;
+
+public class Location {
+
+	private Integer location_id;
+	private String city;
+	
+	public Integer getLocation_id() {
+		return location_id;
+	}
+	public void setLocation_id(Integer location_id) {
+		this.location_id = location_id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
