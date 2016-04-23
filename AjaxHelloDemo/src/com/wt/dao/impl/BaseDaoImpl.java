3a08e179852246b7288f4fc3c0f3139c94f45513
hohoTT <609029365@qq.com>
2016-04-23 16:55:51 +0800

    添加项目中依赖的jar包

--- AjaxHelloDemo/src/com/wt/dao/impl/BaseDaoImpl.java
+++ AjaxHelloDemo/src/com/wt/dao/impl/BaseDaoImpl.java
@@ -2,19 +2,19 @@ package com.wt.dao.impl;
 
 import java.util.List;
 
-import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
-
 import com.wt.beans.Location;
 import com.wt.dao.BaseDao;
 
-public class BaseDaoImpl extends HibernateDaoSupport implements BaseDao{
+public class BaseDaoImpl implements BaseDao{
 
 	@Override
 	public List<Location> getForList(String sql) {
 		
-		List locations = getHibernateTemplate().find(sql);
+//		List locations = getHibernateTemplate().find(sql);
+//		
+//		return locations;
 		
-		return locations;
+		return null;
 	}
 
 }
--- AjaxHelloDemo/src/com/wt/servlet/EmployeeServlet.java
+++ AjaxHelloDemo/src/com/wt/servlet/EmployeeServlet.java
@@ -20,27 +20,29 @@ public class EmployeeServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String methodName = request.getParameter("method");
+		System.out.println("-------AjaxHelloDemo-----");
 		
-		try {
-			Method method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
-			method.invoke(this, request, response);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		String methodName = request.getParameter("method");
+//		
+//		try {
+//			Method method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+//			method.invoke(this, request, response);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
 	}
 
 	private BaseDaoImpl baseDaoImpl = new BaseDaoImpl();
-	
-	protected void listLocations(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		String sql = "SELECT location_id, city FROM Location";
-		
-		List<Location> locations = baseDaoImpl.getForList(sql);
-		
-		request.setAttribute("locations", locations);
-		
-		request.getRequestDispatcher("/WEB-INF/pages/employees.jsp").forward(request, response);
-	}
+//	
+//	public void listLocations(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	
+//		String sql = "SELECT location_id, city FROM Location";
+//		
+//		List<Location> locations = baseDaoImpl.getForList(sql);
+//		
+//		request.setAttribute("locations", locations);
+//		
+//		request.getRequestDispatcher("/WEB-INF/pages/employees.jsp").forward(request, response);
+//	}
 }
